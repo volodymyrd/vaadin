71d56326967181051a88814875b1409e0b3740ca
volodymyrd <volodymyrdotsenko@gmail.com>
2015-10-19 23:22:21 +0300

    cms

---
+++ templates/cms/fe/src/main/java/com/gmail/volodymyrdotsenko/cms/fe/Main.java
@@ -0,0 +1,19 @@
+package com.gmail.volodymyrdotsenko.cms.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Main.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
+}
